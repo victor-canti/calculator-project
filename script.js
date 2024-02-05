54856eb30ffb6626cccc2f0cb0d8c7bcec358136
victor-canti <154830507+victor-canti@users.noreply.github.com>
2024-02-05 09:36:34 -0300

    Add display error message when divide num by 0

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const display = document.querySelector('#display');
-let firstNumber = 0;
+let firstNumber;
 let firstNumStored;
 let operator;
 let secondNumber;
@@ -9,36 +9,45 @@ let currentValue = null;
 // store all nums clicked to concat them instead overwrite
 let displayValue = [];
 const numsBtn = document.querySelectorAll('.number-grid');
-const clearAllBtn = document.querySelector('#clear-all')
 
 numsBtn.forEach(numClick => {
     numClick.addEventListener('click', () => {
+
+        if (display.textContent == 'Error') {
+            display.textContent = 0;
+            displayValue = [];
+            firstNumber = undefined;
+            secondNumber = 0;
+            currentValue = null;
+        }
         
         if (firstNumber !== null) {
             // each num id store their own value, and its added to displayValue
             displayValue.push(numClick.id)
-            display.value = displayValue.join('');
+            display.textContent = displayValue.join('');
             firstNumber = displayValue.join('');
         } 
         // allow to do more than 1 operation with the same number and takes secondNumber as 
         // second parameter of operate() function
         else if (currentValue !== null) {
             displayValue.push(numClick.id)
-            display.value = displayValue.join('');
+            display.textContent = displayValue.join('');
             secondNumber = displayValue.join('');
         } else {
             displayValue.push(numClick.id)
-            display.value = displayValue.join('');
+            display.textContent = displayValue.join('');
             secondNumber = displayValue.join('');
         }
         
     });
 })
 
+const clearAllBtn = document.querySelector('#clear-all')
+
 clearAllBtn.addEventListener('click', () => {
-    display.value = 0;
+    display.textContent = 0;
     displayValue = [];
-    firstNumber = 0;
+    firstNumber = undefined;
     secondNumber = 0;
     currentValue = null;
 })
@@ -48,31 +57,42 @@ basicsOperatorsBtn.forEach(operatorPick => {
     let operatorName = operatorPick.id;
 
     operatorPick.addEventListener('click', () => {
+    
+        // if dividing a number by 0 display error message
+        if (operator == 'divide' && secondNumber == 0) {
+            divide(1, 0);
+            operator = undefined;
+        } else if (currentValue !== null || firstNumber === null) {
 
-        if (currentValue !== null) {
-            if (secondNumber > 0) {
+            if (secondNumber > 0 && currentValue !== null) {
                 operate(Number(currentValue), Number(secondNumber), operator);
                 operator = operatorPick.id;
                 secondNumber = 0;
                 displayValue = [];
-            } else {
-            display.value = currentValue;
+            } else if (secondNumber > 0) {
+                operate(Number(firstNumStored), Number(secondNumber), operator);
+                operator = operatorPick.id;
+                secondNumber = 0;
+                displayValue = [];
+                } else if (operator !== undefined) {
+                display.textContent = currentValue;
+                displayValue = [];
+                operatorName === 'sum' ? operator = 'sum' :
+                operatorName === 'subtract' ? operator = 'subtract' :
+                operatorName === 'multiply' ? operator = 'multiply' :
+                operator = 'divide';
+                }
+
+        } else if (firstNumber !== undefined) {
+            firstNumStored = firstNumber;
+            display.textContent = firstNumber;
             displayValue = [];
+            firstNumber = null;
             operatorName === 'sum' ? operator = 'sum' :
             operatorName === 'subtract' ? operator = 'subtract' :
             operatorName === 'multiply' ? operator = 'multiply' :
             operator = 'divide';
-            }
-        } else {
-        firstNumStored = firstNumber;
-        display.value = firstNumber;
-        displayValue = [];
-        firstNumber = null;
-        operatorName === 'sum' ? operator = 'sum' :
-        operatorName === 'subtract' ? operator = 'subtract' :
-        operatorName === 'multiply' ? operator = 'multiply' :
-        operator = 'divide';
-        }
+        }   
     })
 })
 
@@ -101,25 +121,29 @@ function operate(num1, num2, operator) {
 }
 
 function sum(num1, num2) {
-    display.value = num1 + num2;
+    display.textContent = num1 + num2;
     currentValue = num1 + num2;
     secondNumber = 0;
 }
 
 function subtract(num1, num2) {
-    display.value = num1 - num2
+    display.textContent = num1 - num2
     currentValue = num1 - num2;
     secondNumber = 0;
 }
 
 function multiply(num1, num2) {
-    display.value = num1 * num2
+    display.textContent = num1 * num2
     currentValue = num1 * num2;
     secondNumber = 0;
 }
 
 function divide(num1, num2) {
-    display.value = num1 / num2
-    currentValue = num1 / num2;
-    secondNumber = 0;
+    if (num2 == 0) {
+        display.textContent = 'Error';
+    } else {
+        display.textContent = num1 / num2
+        currentValue = num1 / num2;
+        secondNumber = 0;
+    }
 }
